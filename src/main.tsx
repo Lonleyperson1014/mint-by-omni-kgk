2f92877717a27909781ef36480c2e2c8fcd8280c
Mint by OMNI <96768083+Lonleyperson1014@users.noreply.github.com>
2026-09-17 14:51:34 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css' // Standard Tailwind directives hook include target
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
